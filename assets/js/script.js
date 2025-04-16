babe92d2a1f411d4a8006939047a21010620e444
khushbupatel05 <khushbupatel0009@gmail.com>
2025-04-16 14:00:10 +0530

    commit for ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,21 @@
-let click = document.getElementById("btn")
-let Answer  = document.getElementById("ans")
+let click = document.getElementById("btn");
+let temp = document.getElementById("temp");
+let icon = document.getElementById("icon");
+let description = document.getElementById("description");
+let humidity = document.getElementById("humidity");
+let wind = document.getElementById("wind");
 
-click.addEventListener("click",function(){
-    let cityName = document.getElementById("ctName").value;
+click.addEventListener("click", function () {
+  let cityName = document.getElementById("ctName").value;
+  let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=c7a2b33e4c6e7fff86368c2d79d8ce6f`;
 
-    let URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=c7a2b33e4c6e7fff86368c2d79d8ce6f`;
-    
-    fetch(URL)
-    .then((res)=> {
-        return res.json();
-    })
+  fetch(URL)
+    .then((res) => res.json())
     .then((res) => {
-        Answer.innerHTML = `${res.main.temp} <img src="https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png" alt="" id="icon"> `
-        // console.log(res);  
-    })
-
-})
+      temp.innerHTML = `${res.main.temp}°C`;
+      icon.src = `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
+      description.innerHTML = res.weather[0].description;
+      humidity.innerHTML = `${res.main.humidity}%<br><small>Humidity</small>`;
+      wind.innerHTML = `${res.wind.speed}Km/H<br><small>Wind Speed</small>`;
+    });
+});
