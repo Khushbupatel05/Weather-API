f913f205760b9a75daa1b4fd1498b0227992fdc0
khushbupatel05 <khushbupatel0009@gmail.com>
2025-04-16 16:27:03 +0530

    commit for sweetalert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let click = document.getElementById("btn");
 let temp = document.getElementById("temp");
+let loc = document.getElementById("location");
 let icon = document.getElementById("icon");
 let description = document.getElementById("description");
 let humidity = document.getElementById("humidity");
@@ -12,10 +13,21 @@ click.addEventListener("click", function () {
   fetch(URL)
     .then((res) => res.json())
     .then((res) => {
+      if(res.cod !== 200){
+        Swal.fire({
+          position: "top",
+          text: "City Not Found!",
+        });
+        document.getElementById("ctName").value = "";
+        return;
+      }
+
+      console.log(res);
       temp.innerHTML = `${res.main.temp}°C`;
       icon.src = `https://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png`;
-      description.innerHTML = res.weather[0].description;
-      humidity.innerHTML = `${res.main.humidity}%<br><small>Humidity</small>`;
-      wind.innerHTML = `${res.wind.speed}Km/H<br><small>Wind Speed</small>`;
+      description.innerHTML = res.weather[0].description;  
+      loc.innerHTML = `<i class="bi bi-geo-alt-fill fs-6"></i> ${res.name}`;
+      humidity.innerHTML = `${res.main.humidity}%`;
+      wind.innerHTML = `${res.wind.speed}km/h`;
     });
 });
